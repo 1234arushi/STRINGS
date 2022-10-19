1bbc90647a8860c66ededc221d2fc372108d1bd9
arushisharma <39919147+1234arushi@users.noreply.github.com>
2022-10-19 23:03:51 +0530

    Update 12.cpp

--- GFG/12.cpp
+++ GFG/12.cpp
@@ -1,3 +1,34 @@
+//https://leetcode.com/problems/longest-common-subsequence/
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        
+        int n=text1.size(), m= text2.size();
+        
+        vector<vector<int>> lcs(n+1,vector<int>(m+1,0));
+        
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(text1[i-1]==text2[j-1])//text1[0]==text2[0]
+                {
+                    lcs[i][j]=1+lcs[i-1][j-1];
+                }
+                else
+                {
+                    lcs[i][j]=max(lcs[i-1][j],lcs[i][j-1]);
+                    
+                }
+            }
+        }
+        return lcs[n][m];
+        
+    }
+};
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
 //https://practice.geeksforgeeks.org/problems/longest-common-subsequence-1587115620/1
 // { Driver Code Starts
 #include<bits/stdc++.h>
