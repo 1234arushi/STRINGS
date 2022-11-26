4be5a9b54a180e4c07e1a2ca0125d3b8c9180f01
arushisharma <39919147+1234arushi@users.noreply.github.com>
2022-11-26 10:48:53 +0530

    Create 53.cpp

---
+++ GFG/53.cpp
@@ -0,0 +1,18 @@
+//https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/description/
+
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+        string ans1,ans2;
+        for(int i=0;i<word1.size();i++)
+        {
+            ans1+=word1[i];
+        }
+        for(int i=0;i<word2.size();i++)
+        {
+            ans2+=word2[i];
+        }
+        return ans1==ans2?true:false;
+        
+    }
+};
